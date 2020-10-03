64d8829b6ba26dbc5e8b6c41f4229f3268890ee4
Chris <chriswfoster@gmail.com>
2020-10-03 09:50:14 -0500

    Some more basic setup trying to get massive to work

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 const schedule = require('node-schedule');
+const moment = require('moment');
+var Massive = require("massive");
+const massive = require('massive');
+
+require('dotenv').config();
+process.env['NODE_TLS_REJECT_UNAUTHORIZED'] = 0;
+// var connectionString = "postgres://massive:password@localhost/chinook";
+
+// var db = Massive.connectSync({db : "my_db"});
+// var massiveInstance = massive.connectSync({connectionString : process.env.SQL_CREDZ}) 
+console.log(process.env.SQL_CREDZ)
+let dbInstance;
+massive({connectionString: process.env.SQL_CREDZ, rejectUnauthorized: false, ssl: true})
+.then(massiveInstance => {
+    dbInstance = massiveInstance
+    // app.set('db', massiveInstance);
+    // http.createServer(app).listen(8080);
+})
+.catch(err => console.log("An error: ", err))
 
 
 
 var j = schedule.scheduleJob({second: 10}, function(){
-    console.log('Today is recognized by Rebecca Black!');
-  });
+    console.log("Running the task now, at" , moment(new Date()).format('HH:mm'), ' oclock');
+    if(dbInstance) {
+        dbInstance.getUsers()
+        .then(rsp => console.log(rsp))
+        .catch(err => console.log('an err: ', err))
+    }
+
+});
